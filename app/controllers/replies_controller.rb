c77a4c024d7c790746fc5cb7d42ff41b69581fb2
Kunj <shahkunj42@gmail.com>
2022-08-23 11:39:00 -0400

    routes and controllers

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,2 +1,12 @@
 class RepliesController < ApplicationController
+
+    def create 
+        reply = @current_user.replies.create!(reply_params)
+        render json: reply, status: :created
+    end
+
+    private
+    def reply_params
+        params.permit(:reply, :likes, :cheep_id)
+    end
 end
