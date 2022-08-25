eb6d7423b8a3b9fda4e4306d0100f19bf1b4c027
Jamie <bennythedev@gmail.com>
2022-08-25 08:56:22 -0500

    semantic ui feed formatting

--- client/src/HomeFeedCheepMap.js
+++ client/src/HomeFeedCheepMap.js
@@ -1,11 +1,30 @@
+import { Feed, Icon } from 'semantic-ui-react'
+
 function HomeFeedCheepMap({cheep, user}) {
 
     return (
-        <div>
-            <p>{user.username}</p>
-            <img src={user.profile_image} alt="profile"></img>
-            <p>{cheep.cheep}</p>
-        </div>
+        <Feed>
+            <Feed.Event>
+                <Feed.Label image={user.profile_image} />
+                <Feed.Content>
+                    <Feed.Summary>
+                        <a>{user.username}</a> posted on their page
+                        <Feed.Date>3 days ago</Feed.Date>
+                    </Feed.Summary>
+                    <Feed.Extra text>
+                    {cheep.cheep}
+                    </Feed.Extra>
+                <Feed.Meta>
+                    <Feed.Like>
+                        <Icon name='like' />{user.likes}
+                    </Feed.Like>
+                </Feed.Meta>
+            </Feed.Content>
+        </Feed.Event>
+        </Feed>
+            // <p>{user.username}</p>
+            // <img src={user.profile_image} alt="profile"></img>
+            // <p>{cheep.cheep}</p>
     )
 }
 
