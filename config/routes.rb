1dd78a677e35e5b65c0d50df8c341fed44db9afe
Kunj <shahkunj42@gmail.com>
2022-08-29 18:15:45 -0400

    namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  
-  resources :replies
-  resources :cheeps
-  resources :users
+  namespace :api do
+    resources :replies
+    resources :cheeps
+    resources :users
 
-  post '/newaccount', to: 'users#create'
-  get '/me', to: 'users#show'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/feed', to: 'users#feed'
-  get '/myCheeps', to: 'users#myCheeps'
-  patch '/like/:id', to: 'cheeps#like'
-  patch '/unlike/:id', to: 'cheeps#unlike'
-  post '/follow', to: 'users#follow'
-  
+    post '/newaccount', to: 'users#create'
+    get '/me', to: 'users#show'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    get '/feed', to: 'users#feed'
+    get '/myCheeps', to: 'users#myCheeps'
+    patch '/like/:id', to: 'cheeps#like'
+    patch '/unlike/:id', to: 'cheeps#unlike'
+    post '/follow', to: 'users#follow'
+  end
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
